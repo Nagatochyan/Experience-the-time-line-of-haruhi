85e0d81deba7490ca9d1d5c0a60a7b01f1b850e8
Nagatochyan <94958239+Nagatochyan@users.noreply.github.com>
2022-09-17 21:43:56 +0900

    エンドレスエイト

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ print("客観的視点")
 def julyseventh():
     print("笹の葉ラプソディ:きょんがハルヒの地上絵を手伝いジョンスミスと名乗る")
     print("ハルヒの消失,長門の暴走を止めるために時間遡行する。:緊急脱出プログラム")
+def endlesseight():
+    for i in range(1, 15533):
+        print("エンドレスエイト"+i+"回目")
