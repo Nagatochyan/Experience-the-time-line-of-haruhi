eb2374ca025a4d26fd31070357cb44fbaf6118f0
Nagatochyan <94958239+Nagatochyan@users.noreply.github.com>
2022-09-19 13:05:35 +0900

    main

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-print("客観的視点")
+#キョン視点から見たもの。
+#時間移動など特別な時間現象しか表してません。
+#小説版を入れるといろいろ大変なためアニメ版のみにしました。
+
 def julyseventh():
-    print("笹の葉ラプソディ:きょんがハルヒの地上絵を手伝いジョンスミスと名乗る")
-    print("ハルヒの消失,長門の暴走を止めるために時間遡行する。:緊急脱出プログラム")
+    print("7/7へ時間遡行")
+julyseventh()
+def Bamboo_Leaf_Rhapsody():
+    print("キョンがハルヒの地上絵を手伝うために時間遡行し、ジョンスミスと名乗る。")
+    return julyseventh
+Bamboo_Leaf_Rhapsody()
 def endlesseight():
     for i in range(1, 15533):
         print("エンドレスエイト"+i+"回目")
+endlesseight()
+def modification():
+    print("長門が12月18日に世界を改変してしまう。ハルヒ、長門、朝比奈さん、小泉くん、鶴屋さんなどが色々改変され今までの関係がなくなってしまう。")
+modification()
+def disappearance():
+    print("時間遡行２回目")
+    print("世界の改変を治すためもう一度三年前へ")
+    return julyseventh,modification
+disappearance()
+def save():
+    print("世界を改変しようとした長門に対して改変を治す薬を打ち世界を元に戻す。")
